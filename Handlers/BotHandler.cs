9ea2ee3eeadbf04d5a03fbc270aa892935815d96
Serge <sanichace@gmail.com>
2023-04-03 23:22:10 +0300

    Setup environment

---
+++ Handlers/BotHandler.cs
@@ -0,0 +1,37 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace Digichlist.Bot.Handlers
+{
+    public class BotHandler : IUpdateHandler
+    {
+        /// <summary>
+        /// Entry point of getting Telegram messages.
+        /// </summary>
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            // Exit when cancellation is requested.
+            if(cancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
+
+            // Temp stub.
+            if (update.Message is not null)
+            {
+                var chatId = update.Message.Chat.Id;
+                await botClient.SendTextMessageAsync(chatId, "I know you're here");
+            }
+        }
+
+        /// <summary>
+        /// Handles errors while polling.
+        /// </summary>
+        public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.Error.WriteLine(exception.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Digichlist.Bot.Configuration;
+using Digichlist.Bot.Handlers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+
+var builder = new HostBuilder()
+    .ConfigureServices((context, services) =>
+    {
+        var botToken = Environment.GetEnvironmentVariable("DigichlistV2", EnvironmentVariableTarget.User) ?? throw new ArgumentException("Missing API token for the Telegram Bot.");
+        services.AddSingleton<ITelegramBotClient>(new TelegramBotClient(botToken));
+        services.AddSingleton<IUpdateHandler, BotHandler>();
+    });
+
+var host = builder.Build();
+
+var serviceScope = host.Services.CreateScope();
+
+var services = serviceScope.ServiceProvider;
+
+try
+{
+    var botClient = services.GetService<ITelegramBotClient>();
+    var updateHandler = services.GetService<IUpdateHandler>();
+    botClient.StartReceiving(updateHandler);
+
+    Console.WriteLine($"{Constants.APP_NAME} is all set.");
+    Console.ReadKey();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error Occured: {ex}");
+}
